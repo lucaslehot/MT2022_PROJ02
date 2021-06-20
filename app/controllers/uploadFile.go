b52133488837fb78f9b733ee4efa4b2c64aab983
Lucas Lehot <lehotlucas@gmail.com>
2021-06-20 21:05:24 +0200

    moving code from main to controller

--- app/controllers/uploadFile.go
+++ app/controllers/uploadFile.go
@@ -7,10 +7,24 @@ import (
 )
 
 func UploadFile(w http.ResponseWriter, r *http.Request) {
+	// Setting up Redis connection
+	// connection, err := rmq.OpenConnection("message_broker", "tcp", "localhost:6379", 1, nil)
+	// taskQueue, err := connection.OpenQueue("tasks")
+
+	// redisErr := database.Connect()
+	// if redisErr != nil {
+	// 	log.Fatalf("Impossible de se connecter à la bdd: %v", redisErr)
+	// }
+
+	//-----------------------------
 	fmt.Println("File Upload Endpoint Hit")
 
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
 	r.ParseMultipartForm(10 << 20)
-
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
 	file, handler, err := r.FormFile("myFile")
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
@@ -22,18 +36,45 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
 	tempFile, err := ioutil.TempFile("avatar-upload", "upload-*.png")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer tempFile.Close()
 
+	// read all of the contents of our uploaded file into a
+	// byte array
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	// write this byte array to our temporary file
 	tempFile.Write(fileBytes)
-
+	// return that we have successfully uploaded our file!
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
+
+	//-----------------------------
+	// db := database.DbConn
+
+	// var user models.User
+	// dbErr := db.Where("id = ?", 1).Find(&user)
+	// if dbErr != nil {
+	// 	log.Fatalf("could not serve on port %s", 8080)
+	// }
+
+	// user.AvatarPath = "./" + string(1)
+
+	// task := models.Task{"generate_conversions", 1}
+
+	// taskBytes, err := json.Marshal(task)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// err = taskQueue.PublishBytes(taskBytes)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 }
